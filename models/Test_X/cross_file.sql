f7e72a886a78c22f53c5e21443df1f7a0869138a
Nanduzz99 <prmakki@gmail.com>
2022-03-13 17:45:26 +0530

    4th

--- models/Test_X/cross_file.sql
+++ models/Test_X/cross_file.sql
@@ -1,6 +1,5 @@
 select
-    a.brand_name,
-    b.brand_id
+    a.brand_name
 from {{ ref('custom_test1') }} a
 left join {{ ref('custom_test2') }} b
 on a._airbyte_custom_test1_hashid = b._airbyte_custom_test2_hashid
